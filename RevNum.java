90a36bedb1b45f9dea4d213c1ba775ca877079eb
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-15 07:42:55 +0000

    Create RevNum.java

---
+++ RevNum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class RevNum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int num, rev=0;
+		Scanner s=new Scanner(System.in);
+		num=s.nextInt();
+
+        while(num != 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println(rev);
+	}
+}
