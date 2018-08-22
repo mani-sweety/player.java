2a7a611832fdb8387f57fe857c909cfe10aa98ff
mani-sweety <41941890+mani-sweety@users.noreply.github.com>
2018-08-22 15:49:33 +0530

    Create Set14.java

---
+++ Set14.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Set14
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		StringBuilder sb=new StringBuilder(str);
+		sb.append(".");
+System.out.print(sb);
+	}
+}
